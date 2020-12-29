819614c68f0a42ddb254ce8a2fbf2160feecde55
Chris Falk <christophersfalk@gmail.com>
2020-12-29 10:41:55 -0700

    added axios and API call to randomuser.me

---
+++ employeedirectory/src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASEURL = "https://randomuser.me/api/?results=10&nat=us";
+
+const obj = {
+  search: function () {
+    return axios.get(BASEURL);
+  },
+};
+
+export default obj;
