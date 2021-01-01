4153960ca6c83bf4281de79819bad361370252e1
Chris Falk <christophersfalk@gmail.com>
2021-01-01 12:54:38 -0700

    updated table and removed default table values

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,53 +1,56 @@
-import { Component } from "react"
+import { Component } from "react";
 import Table from "react-bootstrap/Table";
-
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Container from 'react-bootstrap/Container'
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import API from "./utils/API.js";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    search: "",
+    results: [],
+  };
+
+  componentDidMount() {
+    API.getUsers()
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ results: res.data.results });
+      })
+      .catch((err) => console.log(err));
+  }
+
   render() {
-  return (
-    <div className="App">
-      <Jumbotron fluid>
-        <Container>
-          <h1>Employee Tracking System v. 2.7</h1>
-          <p>
-            something. all you base belong to us 
-          </p>
-        </Container>
-      </Jumbotron>
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Jumbotron fluid>
+          <Container>
+            <h1>Employee Tracking System v. 2.7</h1>
+            <p>something. all you base belong to us</p>
+          </Container>
+        </Jumbotron>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.results.map((result, i) => (
+              <tr>
+                <td>{i+1}</td>
+                <td>{result.name.first}</td>
+                <td>{result.name.last}</td>
+                <td>{result.login.username}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
   }
 }
 
